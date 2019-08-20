f8defa18b7933a2d1ea50e11206b0d2c979b76ae
astillo <clomes.alex@gmail.com>
2019-08-19 19:08:20 -0500

    basic testing done

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,24 @@
-// Test away!
+// Test away!// Test away// Test away!
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+import { render, fireEvent } from "@testing-library/react";
+
+import Display from "./Display";
+
+describe("<Display />", () => {
+    // 2. write this test
+    it("matches snapshot", () => {
+        const tree = renderer.create(<Display />); // generates a DOM tree
+
+        // snapshots are a JSON representation of the DOM tree
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+    // it('displays locked when lock is clicked', () => {
+    //     const lock = jest.fn()
+    //     let lockedMock = ''
+    //     let state = { open: true, closed: false, locked: false }
+    //     const { container } = render(<Dashboard />)
+    //     expect(container.getElementsByTagName('button')[0].disabled).toBe(true)
+    //     expect(container.getElementsByTagName('button')[1].disabled).toBe(false)
+    // })
+})
